afb3cec440ebabacd1426ab47165d893e61fc24f
Chris McNabb <chris@unscrambledops.com>
2017-05-24 20:20:33 -0500

    fix issues with limit/maximum, and improve response when no incidents are returned

--- actions/get_incidents.py
+++ actions/get_incidents.py
@@ -2,7 +2,7 @@ from lib.action import PDAction
 
 
 class OpenIncidents(PDAction):
-    def run(self, statuses, limit, sort_by):
+    def run(self, statuses, maximum, sort_by):
         """List incidents with status triggered or acknowledged"""
         status = False
         results = {'incidents': []}
@@ -11,7 +11,7 @@ class OpenIncidents(PDAction):
             if isinstance(statuses, basestring):
                 statuses = [s.strip() for s in statuses.split(',')]
             results['incidents'] = self.pypd.Incident.find(
-                sort_by=sort_by, statuses=statuses, limit=limit)
+                sort_by=sort_by, statuses=statuses, maximum=maximum)
             status = True
         except Exception as e:
             results['error'] = {'type': type(e).__name__, 'message': e.message}
